7becfbc7e9471306e2dcecf4c422667ef709e67a
mikhalexandr <mikhalchenkov22@gmail.com>
2025-11-02 14:55:00 +0300

    feat: lab3

--- lab3/src/Informatics_Lab3_Task2.py
+++ lab3/src/Informatics_Lab3_Task2.py
@@ -1,4 +1,71 @@
 # Author = Mikhalchenkov Alexandr Nikolaevich
 # Group = P3109
 # Date = 31.10.2025
-# test2
+# Variant = 501651 % 5 = 1
+import re
+
+
+def words_with_one_vowel(text):
+    vowels = f"{"аеёиоуыэюя"}{"аеёиоуыэюя".capitalize()}-"
+    words = re.findall(r"\b[а-яА-ЯёЁ-]+\b", text)
+    result = []
+    for word in words:
+        vowels_in_word = re.findall(fr"[{vowels}]", word)
+        if vowels_in_word:
+            if len(set(v.lower() for v in vowels_in_word if v != "-")) == 1:
+                result.append(word)
+    result.sort(key=lambda w: (len(w), w.lower()))
+    return result
+
+
+tests = [
+    {
+        'input': "\t  А у кого другой преподаватель ведёт? Ну, вот он очень старается, чтобы вам было неприятно, "
+                 "но неприятно в хорошем смысле слова, поэтому он придумывает интересные задания\n"
+                 "\t  © Балакшин П.В.",
+        'expected': ["А", "у", "но", "Ну", "он", "он", "вам", "вот", "кого"]
+    },
+    {
+        'input': "\t  С: А можно аннотацию на камне принести?\n"
+                 "\t  П: На надгробном?..\n"
+                 "\t  © Балакшин П.В.",
+        'expected': ["А", "на", "На", "можно"]
+    },
+    {
+        'input': "\t  П: Что-то вас сегодня мало... Значит придётся применять репрессивные меры.\n"
+                 "\t  С: Какие например?\n"
+                 "\t  П: Пороть.\n"
+                 "\t  С: Тех, кто пришёл?\n"
+                 "\t  П: Ну, а вдруг вам понравится...\n"
+                 "\t  © Балакшин П.В.",
+        'expected': ["а", "Ну", "вам", "вас", "кто", "Тех", "вдруг", "Пороть", "Что-то"]
+    },
+    {
+        'input': "\t  В основном я шучу про ПСЖ, но не только\n"
+                 "\t  © Балакшин П.В.",
+        'expected': ["я", "не", "но", "про", "шучу", "только", "основном"]
+    },
+    {
+        'input': "\t  Что-то вас много, надо кого-нибудь отчислить...\n"
+                 "\t  © Балакшин П.В.",
+        'expected': ["вас", "много", "Что-то"]
+    },
+]
+
+all_passed = True
+for index, test in enumerate(tests, 1):
+    result = words_with_one_vowel(test["input"])
+    print(f"Тест {index}:")
+    print(f"\tВход:\n{test["input"]}")
+    print(f"\tОжидается: {", ".join(test["expected"])}")
+    print(f"\tНайдено: {", ".join(result)}")
+    if result != test["expected"]:
+        print("Не cовпадает\n")
+        all_passed = False
+    else:
+        print("Cовпадает\n")
+
+if all_passed:
+    print("Все тесты пройдены успешно!")
+else:
+    print("Что-то пошло не так!")
--- lab3/src/Informatics_Lab3_Task3.py
+++ lab3/src/Informatics_Lab3_Task3.py
@@ -1,3 +1,42 @@
 # Author = Mikhalchenkov Alexandr Nikolaevich
 # Group = P3109
 # Date = 31.10.2025
+# Variant = 501651 % 3 = 0
+import re
+
+CRON_FIELD = r"(?:\*|\*\/\d+|\d{1,2}(?:-\d{1,2})?(?:/\d+)?(?:,\d{1,2}(?:-\d{1,2})?(?:/\d+)?)*)"
+CRON_REGEX = re.compile(
+    r"^" + r"\s+".join([CRON_FIELD]*5) + r"$"
+)
+
+
+def is_valid_cron(expression):
+    return bool(CRON_REGEX.match(expression.strip()))
+
+
+tests = [
+    ("30 14 * * *", True),              # в 14:30 каждый день
+    ("*/5 * * * *", True),              # каждые 5 минут
+    ("0 0 1 1 *", True),                # в полночь 1 января
+    ("1-10/2 0 1,15 * 1-5", True),      # сложное выражение
+    ("12 8 * *", False),                # только 4 поля
+    ("* * * * * *", False),             # 6 полей
+    ("0 0 1-32 1 *", True),             # формат разрешён, диапазон не проверяется
+    ("5,10,15 0-23 * 1-12 0-6", True),  # несколько значений и диапазоны
+    ("* * * * ", False),                # пробел в конце, 4 поля
+]
+
+all_passed = True
+tmp = 0
+for expression, expected in tests:
+    tmp += 1
+    result = is_valid_cron(expression)
+    print(f"Тест {tmp}: {expression:25} -> {result}, ожидается: {expected},"
+          f" {"Совпадает" if result == expected else "Не совпадает"}")
+    if result != expected:
+        all_passed = False
+
+if all_passed:
+    print("Все тесты пройдены успешно!")
+else:
+    print("Что-то пошло не так!")
